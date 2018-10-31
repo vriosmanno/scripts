94912e61f4888c9353486221b67332fa7053a752
Vincent Rios-Manno <vriosmanno@novetta.com>
2018-10-31 06:20:59 -0400

    Update Legion_FaceIndex.sh

--- legion/Legion_FaceIndex.sh
+++ legion/Legion_FaceIndex.sh
@@ -3,5 +3,6 @@
 FACEINDEX_SERVER=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1 -d'/')
 echo "Setting FACE_RECOGNITION_SERVER to: $FACEINDEX_SERVER"
 LEGION_ENV=$GOPATH/src/github.com/Novetta/legion/dev/legion.env
+echo "$LEGION_ENV"
 sed -i.bak '/FACE_RECOGNITION_URL/{$s/\(\([0-9]\{1,3\}\)\(\.\|\:\)\)\{1,4\}/'"$FACEINDEX_SERVER"':/}' "$LEGION_ENV"
 cat $GOPATH/src/github.com/Novetta/legion/dev/legion.env | grep FACE_RECOGNITION
