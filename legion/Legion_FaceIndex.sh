28ce3fb11bbb80375ea9e883ae085d41867cb91f
Vincent Rios-Manno <vriosmanno@novetta.com>
2018-10-31 06:12:27 -0400

    Update Legion_FaceIndex.sh

--- legion/Legion_FaceIndex.sh
+++ legion/Legion_FaceIndex.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 # Default Server
-FACEINDEX_SERVER=10.0.0.41
-
-while [ "$1" != "" ]; do
-    KEY="$1"
-    
-    case $KEY in
-      -l)
-        FACEINDEX_SERVER=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1 -d'/')
-      ;;
-      -i)
-        FACEINDEX_SERVER="$2"
-      ;;
-    esac
-done
+FACEINDEX_SERVER=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1 -d'/')
 
 echo "Setting FACE_RECOGNITION_SERVER to: $FACEINDEX_SERVER"
 LEGION_ENV=$GOPATH/src/github.com/Novetta/legion/dev/legion.env
