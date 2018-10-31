0c4b9de450ea0d06841d2a9405d1ed5ac3e63223
Vincent Rios-Manno <vriosmanno@novetta.com>
2018-10-31 01:40:12 -0400

    Update Legion_FaceIndex.sh

--- legion/Legion_FaceIndex.sh
+++ legion/Legion_FaceIndex.sh
@@ -4,7 +4,7 @@ FACEINDEX_SERVER=10.0.0.41
 
 if $1 == "l"
     FACEINDEX_SERVER=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1 -d'/')
-else if $1 == "i"
+elif $1 == "i"
     FACEINDEX_SERVER=$2
 esac
 done
