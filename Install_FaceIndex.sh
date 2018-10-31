c06c63a5f14ae24a73c5b6d8fa305fa02db5c248
Vincent Rios-Manno <vriosmanno@novetta.com>
2018-10-30 22:45:12 -0400

    Update Install_FaceIndex.sh

--- Install_FaceIndex.sh
+++ Install_FaceIndex.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
+# This sets up FaceIndex locally on your VM
 FACEINDEX_ENV=/opt/faceindex/conf/faceindex.env
- 
-echo "Installing faceindex to local vm"
+
+echo "--- Installing FaceIndex to local VM ---"
 sudo yum install -y faceindex
 /opt/faceindex/setup/updateDb.sh
- 
+
 if grep -qF "PYTHONPATH" $FACEINDEX_ENV; then
-    echo "PYTHONPATH exists in faceindex.env"
-    echo "Updating variable"
-    sudo sed -i.bak '/PYTHONPATH/{$s/=.*/=\/opt\/faceindex\/lib_cpu\/python2.7\/site-packages:$PYTHONPATH/}' $FACEINDEX_ENV
+	echo "PYTHONPATH exists in faceindex.env, updating variable"
+	sudo sed -i.bak '/PYTHONPATH/{$s/=.*/=\/opt\/faceindex\/lib_cpu\/python2.7\/site-packages:$PYTHONPATH/}' $FACEINDEX_ENV
 else
-    echo "PYTHONPATH did not exist in faceindex.env"
-    exho "Adding variable"
-    echo 'export PYTHONPATH=/opt/faceindex/lib_cpu/python2.7/site-packages:$PYTHONPATH' | sudo tee -a $FACEINDEX_ENV
+	echo "PYTHONPATH did not exist in faceindex.env, adding variable"
+	echo 'export PYTHONPATH=/opt/faceindex/lib_cpu/python2.7/site-packages:$PYTHONPATH' | sudo tee -a $FACEINDEX_ENV
 fi
- 
-echo "Complete"
+
+echo "Restarting services"
+sudo systemctl restart faceindex
+sudo systemctl restart kerbproxy
+
+echo "--- Installation Complete ---"
